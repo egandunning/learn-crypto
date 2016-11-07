f7d2b0a1994221903f307bbcdcde08178ff65038
passanpm <passanpm@plu.edu>
2016-11-06 19:41:01 -0800

    commented out second main

--- CapstoneCryptography/src/main.cpp
+++ CapstoneCryptography/src/main.cpp
@@ -1,11 +1,11 @@
 #include "headers/mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
+/*int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
-}
+} */
