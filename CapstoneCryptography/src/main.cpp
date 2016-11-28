85fe2bd2337dc8ba7e82a36f56c55a9c5c21d7d2
passanpm <passanpm@plu.edu>
2016-11-27 23:39:00 -0800

    Window resized to 1000 by 1000

--- CapstoneCryptography/src/main.cpp
+++ CapstoneCryptography/src/main.cpp
@@ -75,6 +75,7 @@ int main(int argc, char *argv[])
 
 		QApplication a(argc, argv);
 		MainWindow w;
+        w.resize(1000, 1000);
 		w.show();
 
 		return a.exec();
--- CapstoneCryptography/src/mainwindow.cpp
+++ CapstoneCryptography/src/mainwindow.cpp
@@ -12,7 +12,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
 }
 
 MainWindow::~MainWindow()
