eb016fcc0d6790f943fba10ecda85e6344b95428
quintonamore <teas98@gmail.com>
2017-02-16 23:54:44 -0800

    Created the manualFindLetters function to test the Keyword Cipher.

--- CapstoneCryptography/test/testcipher.cpp
+++ CapstoneCryptography/test/testcipher.cpp
@@ -1,5 +1,6 @@
 
 #include <QtTest/QtTest>
+#include <string>
 #include </home/onetrueq/learn-crypto/CapstoneCryptography/headers/cipher.h>
 
 
@@ -50,26 +51,40 @@ void TestCipher::basicFindLetters(){
 
 void TestCipher::manualFindLetters(){
 
-    Cipher tc = Cipher("JOB","TEST");
-    int count[26];
-
-    for(int i = 0; i<26; i++){
-        count[i] = 0;
-    }
-
-    for(int i = 0; i<26; i++){
-        count[tc.getChar(i + 65) - 65] = count[tc.getChar(i + 65) - 65] + 1;
-    }
-
-    bool check = true;
-
-    for(int i = 0; i<26; i++){
-        if(count[i] > 1){
-            check = false;
-            break;
+    for(int i = 0; i < 10000; i++){
+        int a1 = rand()%25;
+        int a2 = rand()%25;
+        if(a1 == a2){
+            continue;
         }
+
+        char b1 = 'A' + a1;
+        char b2 = 'A' + a2;
+        std::string t = "";
+        t += b1;
+        t += b2;
+
+        Cipher tc = Cipher(t,"TEST");
+        int count[26];
+
+        for(int i = 0; i<26; i++){
+            count[i] = 0;
+        }
+
+        for(int i = 0; i<26; i++){
+            count[tc.getChar(i + 65) - 65] = count[tc.getChar(i + 65) - 65] + 1;
+        }
+
+        bool check = true;
+
+        for(int i = 0; i<26; i++){
+            if(count[i] > 1){
+                check = false;
+                break;
+            }
+        }
+        QVERIFY(check);
     }
-    QVERIFY(check);
 }
 
 
