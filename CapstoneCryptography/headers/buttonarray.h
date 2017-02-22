72fb965e76aefc43c0fc990312f10298e388568d
passanpm <passanpm@plu.edu>
2017-02-21 18:30:43 -0800

    added to ButtonArray header

--- CapstoneCryptography/headers/buttonarray.h
+++ CapstoneCryptography/headers/buttonarray.h
@@ -1,5 +1,15 @@
 #ifndef BUTTONARRAY_H
 #define BUTTONARRAY_H
 
+class ButtonArray{
+
+private:
+    QPushButton *buttonArray;
+
+pubic:
+    ButtonArray(std::string);
+    std::string makeGuess(std::char, std::char);
+};
+
 #endif // BUTTONARRAY_H
 
