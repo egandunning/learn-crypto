65a60df4609eec67d0b8a1149cd9d18a50f26b48
passanpm <passanpm@plu.edu>
2017-02-21 19:20:24 -0800

    tried to fix the buttonArray classes

--- CapstoneCryptography/headers/buttonarray.h
+++ CapstoneCryptography/headers/buttonarray.h
@@ -1,10 +1,15 @@
 #ifndef BUTTONARRAY_H
 #define BUTTONARRAY_H
 
+#include <string>
+#include "QPushButton"
+#include "QWidget"
+
 class ButtonArray{
 
 private:
     QPushButton *buttonArray;
+    std::string scrambledWord;
 
 public:
     ButtonArray(std::string, QWidget*);
--- CapstoneCryptography/src/buttonarray.cpp
+++ CapstoneCryptography/src/buttonarray.cpp
@@ -1,19 +1,24 @@
 #include <headers/buttonarray.h>
+#include <string>
+#include "QPushButton"
+#include "QWidget"
+#include "QString"
 
 ButtonArray::ButtonArray(std::string s, QWidget* ui){
 
     buttonArray = new QPushButton[s.length()];
+    scrambledWord = s;
 
     for(int i=0; i<s.length(); i++){
-        buttonArray[i] = new QPushButton(s.at(i), ui);
+       // buttonArray[i] = new QPushButton(new QString(s.at(i)), ui);
     }
 
 }
 
-string ButtonArray::makeGuess(char original, char guess){
-    for(int i=0; i<buttonArray.size(); i++){
-        if(buttonArray[i]->text().at(0) == guess){
-            buttonArray[i]->setText(guess);
+std::string ButtonArray::makeGuess(char original, char guess){
+    for(int i=0; i<scrambledWord.length(); i++){
+        if(buttonArray[i].text().at(0) == guess){
+           // buttonArray[i].setText(new QString(guess));
         }
     }
     return "";
