6d2328f8c51021d23157122461dbd01ab707d9fc
passanpm <passanpm@plu.edu>
2017-02-22 19:17:33 -0800

    Now using a QVector not an array

--- CapstoneCryptography/src/buttonarray.cpp
+++ CapstoneCryptography/src/buttonarray.cpp
@@ -1,23 +1,25 @@
 #include <headers/buttonarray.h>
 #include <string>
-#include "QPushButton"
-#include "QWidget"
-#include "QString"
+#include <QPushButton>
+#include <QWidget>
+#include <QString>
+#include <QChar>
+#include <QVector>
 
 ButtonArray::ButtonArray(std::string s, QWidget* ui){
 
-    buttonArray = new QPushButton[s.length()];
     scrambledWord = s;
 
     for(int i=0; i<s.length(); i++){
-       // buttonArray[i] = new QPushButton(new QString(s.at(i)), ui);
+        //QPushButton *p = new QPushButton("philip", ui);
+       //buttonVector.append(*p);
     }
 
 }
 
 std::string ButtonArray::makeGuess(char original, char guess){
     for(int i=0; i<scrambledWord.length(); i++){
-        if(buttonArray[i].text().at(0) == guess){
+        if(buttonVector.at(i).text().at(0) == guess){
            // buttonArray[i].setText(new QString(guess));
         }
     }
