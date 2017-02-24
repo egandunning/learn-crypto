4abdd43365b3b682a612781cec0230f858580565
quintonamore <teas98@gmail.com>
2017-02-23 19:09:02 -0800

    Added a method to easiliy add labels to the x and y axes.

--- CapstoneCryptography/headers/graphwindow.h
+++ CapstoneCryptography/headers/graphwindow.h
@@ -7,6 +7,7 @@
 #include <QLine>
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QGraphicsTextItem>
 #include <vector>
 
 class GraphWindow
@@ -21,7 +22,8 @@ private:
     QGraphicsScene* scene;
     std::vector<QPointF> points;
     void draw();
-    void addTicks(int yMax, int xMax, int numTicks);
+    void addTicks(int yMax, int xMax, int ticksX, int ticksY);
+    void addLabels(std::string ylabel, std::string xlabel, int yMax, int xMax);
     QPointF transform(QPointF);
 };
 
