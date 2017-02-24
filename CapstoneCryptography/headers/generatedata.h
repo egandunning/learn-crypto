e8ccfc1246e1cc4aaf17568dc70df8cb3b41a1f2
Egan <dunninea@plu.edu>
2017-02-23 17:37:58 -0800

    started data point generation function

---
+++ CapstoneCryptography/headers/generatedata.h
@@ -0,0 +1,15 @@
+#ifndef GENERATEDATA_H
+#define GENERATEDATA_H
+
+#include <vector>
+#include <iostream>
+#include <functional>
+
+#include <QPointF>
+#include <gmpxx.h>
+
+class GenerateData {
+    static std::vector<QPointF> generate(std::function<QPointF (mpz_class)>, std::vector<mpz_class>);
+};
+
+#endif
