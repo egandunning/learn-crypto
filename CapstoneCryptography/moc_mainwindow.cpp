82a3faf264e80ead70fd1f52a086f7dba274a56c
passanpm <passanpm@plu.edu>
2017-03-17 13:34:12 -0700

    No changes

--- CapstoneCryptography/moc_mainwindow.cpp
+++ CapstoneCryptography/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[12];
-    char stringdata0[265];
+    QByteArrayData data[14];
+    char stringdata0[331];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -40,7 +40,9 @@ QT_MOC_LITERAL(7, 140, 21), // "on_hashButton_clicked"
 QT_MOC_LITERAL(8, 162, 27), // "on_randomSaltButton_clicked"
 QT_MOC_LITERAL(9, 190, 22), // "on_crackButton_clicked"
 QT_MOC_LITERAL(10, 213, 24), // "on_drawFactoring_clicked"
-QT_MOC_LITERAL(11, 238, 26) // "on_plotCrackButton_clicked"
+QT_MOC_LITERAL(11, 238, 26), // "on_plotCrackButton_clicked"
+QT_MOC_LITERAL(12, 265, 31), // "on_hashLogScaleCheckBox_clicked"
+QT_MOC_LITERAL(13, 297, 33) // "on_factorLogScaleCheckBox_cli..."
 
     },
     "MainWindow\0on_factorPrimesButton_clicked\0"
@@ -49,7 +51,9 @@ QT_MOC_LITERAL(11, 238, 26) // "on_plotCrackButton_clicked"
     "on_random_composite_clicked\0"
     "on_hashButton_clicked\0on_randomSaltButton_clicked\0"
     "on_crackButton_clicked\0on_drawFactoring_clicked\0"
-    "on_plotCrackButton_clicked"
+    "on_plotCrackButton_clicked\0"
+    "on_hashLogScaleCheckBox_clicked\0"
+    "on_factorLogScaleCheckBox_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +63,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,18 +71,22 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   64,    2, 0x08 /* Private */,
-       3,    0,   65,    2, 0x08 /* Private */,
-       4,    0,   66,    2, 0x08 /* Private */,
-       5,    0,   67,    2, 0x08 /* Private */,
-       6,    0,   68,    2, 0x08 /* Private */,
-       7,    0,   69,    2, 0x08 /* Private */,
-       8,    0,   70,    2, 0x08 /* Private */,
-       9,    0,   71,    2, 0x08 /* Private */,
-      10,    0,   72,    2, 0x08 /* Private */,
-      11,    0,   73,    2, 0x08 /* Private */,
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    0,   81,    2, 0x08 /* Private */,
+      10,    0,   82,    2, 0x08 /* Private */,
+      11,    0,   83,    2, 0x08 /* Private */,
+      12,    0,   84,    2, 0x08 /* Private */,
+      13,    0,   85,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -109,6 +117,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 7: _t->on_crackButton_clicked(); break;
         case 8: _t->on_drawFactoring_clicked(); break;
         case 9: _t->on_plotCrackButton_clicked(); break;
+        case 10: _t->on_hashLogScaleCheckBox_clicked(); break;
+        case 11: _t->on_factorLogScaleCheckBox_clicked(); break;
         default: ;
         }
     }
@@ -140,13 +150,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 10;
+        _id -= 12;
     }
     return _id;
 }
